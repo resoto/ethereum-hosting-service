de902f78a103910953b10f03738c831d4c77df48
resoto <makiinumkou@icloud.com>
2020-02-17 21:02:41 +0900

    Update HostingService.sol

--- HostingService.sol
+++ HostingService.sol
@@ -1,49 +1,57 @@
 pragma solidity ^0.5.0;
 
 contract HostingService {
-    string private host;
     
     address owner;
-    mapping(address => string) public html;
-    mapping(address => string) public css;
-    mapping(address => string) public script;
+    
+    struct Page {
+        string html;
+        string css;
+        string script;
+    }
+    
+    mapping(address => Page) public pages;
+    
+    
     
     constructor () public {
         owner = msg.sender;
     }
     
     function getHtml(address _address) public view returns (string memory) {
-        return html[_address];
+        return pages[_address].html;
     }
     
     function getCss(address _address) public view returns (string memory) {
-        return css[_address];
+        return pages[_address].css;
     }
     
     function getScript(address _address) public view returns (string memory) {
-        return script[_address];
+        return pages[_address].html;
     }
     
     function setHtml(string memory _html) public  {
-        html[msg.sender] = _html;
+        pages[msg.sender].html = _html;
     }
     
     function setCss(string memory _css) public  {
-        css[msg.sender] = _css;
+        pages[msg.sender].css = _css;
     }
     
     function setScript(string memory _script) public  {
-        script[msg.sender] = _script;
+        pages[msg.sender].script = _script;
     }
     
     function deletePage(address _address) public onlyOwner {
-        html[_address] = "delete page";
-        css[_address] = "";
-        script[_address] = "";
+        pages[_address].html = "delete page";
+        pages[_address].css = "";
+        pages[_address].script = "";
     }
     
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
+    
+    
 }
