be76d4378d47358e6eecd23f37ab87c4cd8d8b24
resoto <makiinumkou@icloud.com>
2020-02-17 21:03:35 +0900

    Update NameSystem.sol

--- NameSystem.sol
+++ NameSystem.sol
@@ -1,1 +1,49 @@
-aa
+pragma solidity ^0.5.0;
+
+contract NameSystem {
+    
+    address owner;
+    struct NameSet {
+        address hostAddress;
+        string hostname;
+    }
+    
+    NameSet[] nameSets;
+    
+    constructor () public {
+        owner = msg.sender;
+    }
+    
+    function getHostAddress(string memory _hostname) public view returns (string memory) {
+        for(uint i=0;i<nameSets.length;i++){
+            if(keccak256(bytes(nameSets[i].hostname))== keccak256(bytes(_hostname))){
+                return nameSets[i].hostname;
+            }
+        }
+        return "Address Not Found";
+    }
+    
+    function getHostname(address _hostAddress) public view returns (address) {
+        for(uint i=0;i<nameSets.length;i++){
+            if(nameSets[i].hostAddress == _hostAddress){
+                return nameSets[i].hostAddress;
+            }
+        }
+    }
+    
+    function setHostname(string memory _hostname) public {
+        
+        for(uint i=0;i<nameSets.length;i++){
+            if(nameSets[i].hostAddress == msg.sender){
+                nameSets[i].hostname = _hostname;
+                return;
+            }
+        }
+        
+        for(uint i=0;i<nameSets.length;i++){
+            require(keccak256(bytes(nameSets[i].hostname))== keccak256(bytes(_hostname)));
+        }
+        nameSets.push(NameSet(msg.sender,_hostname));
+    }
+    
+}
